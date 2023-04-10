bde025d293a927f3ce55490ef884f617b8dfaf64
sefeoluwa <sefeoluwa01@gmail.com>
2023-04-10 17:26:28 +0100

    javascript object testing

---
+++ script.js
@@ -0,0 +1,30 @@
+const writeOut = document.querySelector(".info");
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+
+        this.info = function () {
+            let readStatus = "not read yet";
+            if (this.read) {
+                readStatus = "already read";
+            }
+            return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}.`;
+        };
+    }
+}
+
+
+
+  // create a new book object
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 304, true);
+
+const book2 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 500, false );
+writeOut.textContent = book2.info();
+
+// call the info() method to get the book's information
+writeOut.textContent = book1.info(); // "The Hobbit by J.R.R. Tolkien, 304 pages, already read."
+
